93725f5bff35249539ba221ddfeca1e5d265615b
Jason Tsai <tez125@g.ncu.edu.tw>
2017-04-19 23:04:41 +0800

    Initial Commit

---
+++ TMPhoto/TMPhoto.h
@@ -0,0 +1,19 @@
+//
+//  TMPhoto.h
+//  TMPhoto
+//
+//  Created by Jason Tsai on 2017/4/19.
+//  Copyright © 2017年 YomStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMPhoto.
+FOUNDATION_EXPORT double TMPhotoVersionNumber;
+
+//! Project version string for TMPhoto.
+FOUNDATION_EXPORT const unsigned char TMPhotoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMPhoto/PublicHeader.h>
+
+
